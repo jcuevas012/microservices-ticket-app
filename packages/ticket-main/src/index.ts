6fa7a0db5fa850518f3b084abcc2e8bdd8899631
Jorge Cuevas <jcq012@gmail.com>
2022-03-08 09:17:48 -0400

    feat: ticket main service

--- packages/ticket-main/src/index.ts
+++ packages/ticket-main/src/index.ts
@@ -37,9 +37,12 @@ const start = async () => {
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined.')
     }
+    if (!process.env.MONGO_URI) {
+        throw new Error('JWT_KEY must be defined.')
+    }
 
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/ticket', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
