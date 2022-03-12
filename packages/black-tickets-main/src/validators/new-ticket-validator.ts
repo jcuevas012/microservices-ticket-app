b65dfdd35d7930144dfff3980b22bae253127a5f
Jorge Cuevas <jcq012@gmail.com>
2022-03-12 00:29:38 -0400

    feat: tickets routes setup

--- packages/black-tickets-main/src/validators/new-ticket-validator.ts
+++ packages/black-tickets-main/src/validators/new-ticket-validator.ts
@@ -2,5 +2,5 @@ import { body } from 'express-validator'
 
 export default [
     body('title').isString().notEmpty().withMessage('Ticket title is not valid'),
-    body('price').isInt().withMessage('Ticket price is not valid'),
+    body('price').isFloat({ gt: 0 }).withMessage('Ticket price is not valid'),
 ]
