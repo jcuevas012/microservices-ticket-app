e139220f6d45db597b962c6b211ee7d85186bd7c
Jorge Cuevas <jcq012@gmail.com>
2022-05-03 00:37:34 -0400

    feat: complete create order handler

--- packages/black-tickets-order/src/routes/new.ts
+++ packages/black-tickets-order/src/routes/new.ts
@@ -1,18 +1,57 @@
-import { requestValidator, requireAuth } from '@black-tickets/utils'
+import { BadRequestError, NotFoundError, requestValidator, requireAuth } from '@black-tickets/utils'
 import { body } from "express-validator"
 import express, { Request, Response } from 'express'
 import * as mongoose  from 'mongoose'
+import { Ticket } from '../models/ticket'
+import {Order, OrderStatus} from '../models/order'
 // import Ticket from '../models/ticket'
 
 const router = express.Router()
+
+const EXPIRATION_SECONDS = 15 * 60
 
 router.post('/', requireAuth, [
     body("ticketId")
     .notEmpty()
     .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
     .withMessage("ticket id must be provided"),
-], requestValidator, async (_req: Request, res: Response) => {
-    res.status(200).send({})
+], requestValidator, async (req: Request, res: Response) => {
+
+    const { ticketId } = req.body
+    // Find existing ticket to order
+
+    const ticket = await Ticket.findById(ticketId)
+
+    if (!ticket) {
+        throw new NotFoundError('ticket not found')
+    }
+
+
+    // check if the ticket was reserved in order completed, created or awaiting for payment
+    const isReserved = await ticket.isReserved()
+
+    if (isReserved) {
+        throw new BadRequestError('ticket is already booked/reserved')
+    }
+
+    // Get expiration time for the created order
+    
+    const expirationTime = new Date()
+    expirationTime.setSeconds(expirationTime.getSeconds() + EXPIRATION_SECONDS)
+
+
+    // create the order
+
+    const order = await Order.build({
+        userId: req.currentUser.id,
+        status: OrderStatus.Created,
+        expiredAt: expirationTime.toString(),
+        ticket
+    })
+
+    await order.save()
+
+    res.status(201).send(order)
 })
 
 export { router as createOrder }
