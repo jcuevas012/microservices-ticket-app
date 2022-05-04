a5f1c074af796e530be4e508ab16a51bcc74fd59
Jorge Cuevas <jcq012@gmail.com>
2022-05-04 00:36:17 -0400

    feat: add order events created and cancelled

--- packages/black-tickets-utils/src/types/index.ts
+++ packages/black-tickets-utils/src/types/index.ts
@@ -1,8 +1,36 @@
+export * from './order.status';
+import {OrderStatus} from './order.status';
+import {Subjects} from './subjects';
+
 interface Ticket {
   id: string;
   title: string;
   price: number;
   userId: string;
+}
+
+interface Order {
+  id: string;
+  status: OrderStatus;
+  expiredAt: string;
+  userId: string;
+  ticket: {
+    id: string;
+    price: number;
+  };
+}
+
+export interface OrderCreatedEvent {
+  subject: Subjects.ORDER_CREATED;
+  data: Order;
+}
+
+export interface OrderCancelledEvent {
+  subject: Subjects.ORDER_CANCELLED;
+  data: {
+    id: string;
+    ticketId: string;
+  };
 }
 
 export interface TicketCreatedEvent {
@@ -15,15 +43,9 @@ export interface TicketUpdatedEvent {
   data: Ticket;
 }
 
-export enum Subjects {
-  TICKET_CREATED = 'ticket:created',
-  TICKET_UPDATED = 'ticket:updated',
-  ORDER_UPDATED = 'order:updated',
-}
-
 export interface Event {
   subject: Subjects;
   data: any;
 }
 
-export * from './order.status';
+export {Subjects};
