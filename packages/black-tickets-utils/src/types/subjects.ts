ad381c7d64c22adcf331bbf442e8a78a56b12cae
Jorge Cuevas <jcq012@gmail.com>
2022-05-04 00:36:36 -0400

    feat: update subjects for orders and relocated type

---
+++ packages/black-tickets-utils/src/types/subjects.ts
@@ -0,0 +1,9 @@
+enum Subjects {
+  TICKET_CREATED = 'ticket:created',
+  TICKET_UPDATED = 'ticket:updated',
+
+  ORDER_CREATED = 'order:created',
+  ORDER_CANCELLED = 'order:cancelled',
+}
+
+export {Subjects};
