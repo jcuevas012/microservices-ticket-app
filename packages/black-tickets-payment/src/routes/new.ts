861d9a47b561c64ab643469bc59003ff8569a6f8
Jorge Cuevas <jcq012@gmail.com>
2022-06-09 00:07:07 -0400

    feat: stripe setup

--- packages/black-tickets-payment/src/routes/new.ts
+++ packages/black-tickets-payment/src/routes/new.ts
@@ -1,6 +1,8 @@
-import { requestValidator, requireAuth } from '@black-tickets/utils'
+import { NotAuthorizedError, NotFoundError, requestValidator, requireAuth, OrderStatus, BadRequestError } from '@black-tickets/utils'
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
+import { Order } from '../models/order'
+import { stripe } from '../stripe'
 
 const router = express.Router()
 
@@ -15,8 +17,30 @@ router.post('/', [
     .withMessage('Should provide a valid orderId')
 
 ], requestValidator,  requireAuth, async (req: Request, res: Response) => {
-    
 
+    const { token , orderId } = req.body
+
+    const order = await Order.findById(orderId)
+
+    if (!order) {
+        throw new NotFoundError('Not order found')
+    }
+    
+    if (order.userId !== req.currentUser?.id) {
+        throw new NotAuthorizedError()
+    }
+
+    if (order.status === OrderStatus.Cancelled) {
+        throw new BadRequestError('Can not process payment for cancelled order')
+    }
+
+
+    await stripe.charges.create({
+        amount: order.price * 100,
+        currency: 'usd',
+        source: token,
+        description: 'Ticket app buy'
+    })
 
     res.status(200).send({ success: true })
 })
